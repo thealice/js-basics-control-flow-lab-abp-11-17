0a89ebc14059f3edcb6d2e3ffeb50a12ae6e50a8
Alice Freda <alicefreda@gmail.com>
2017-11-11 18:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return `This one is on me!`;
+  } else if (feet > 2500) {
+    return `No can do.`;
+  } else {
+    return `I will gladly take your thirty bucks.`;
+  }
+}
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+}
+function switchOnCharmFromTip(tip) {
+  
+  switch (tipAmount) {
+    case 'generous':
+      return ('Thank you so much.');
+      break;
+    case 'notAsGenerous':
+      return ('Thank you.');
+      break;
+    default:
+      return ('Bye.');
+      break;
+  }
+}
